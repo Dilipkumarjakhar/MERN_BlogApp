7e07c5105d7fbaaf9a3f46038c68be4bdda8a78d
DILIP KUMAR JAKHAR <38867706+Dilipkumarjakhar@users.noreply.github.com>
2022-06-06 15:53:06 +0530

    backend & frontend added

--- Frontend/reactblogs/src/App.js
+++ Frontend/reactblogs/src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {Routes,Route} from 'react-router-dom';
+import {Headers}  from './components/Headers';
+import {Login} from './components/Login';
+import {Blog} from './components/Blog';
+import {BlogsDtails} from './components/Blogdetails';
+import {UserBlogs} from './components/UserBlog';
+import {Postblog} from './components/Addblog';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
 
+  const isLoggedIn=useSelector(state=>state.isLoggedIn)
+  console.log('isLoggedIn:', isLoggedIn)
+  return (
+     <React.Fragment>
+     <header>
+
+    <Headers/>
+    </header>
+    <main>
+
+   
+   
+    <Routes>
+  
+      <Route path='/login' element={<Login/>} exact/> 
+      <Route path='/blogs' element={<Blog/>} exact/>
+      <Route path='/myblogs' element={<UserBlogs/>} exact/>
+      <Route path='/myBlogs/:id' element={<BlogsDtails/>} exact/> 
+      <Route path='/blog/add' element={<Postblog/>} exact/> 
+    </Routes>
+    
+    </main> 
+   
+ 
+ </React.Fragment> 
+  )
+ 
+
+}
 export default App;
+
---
+++ Frontend/reactblogs/src/components/Blogdetails.jsx
@@ -0,0 +1,9 @@
+export const BlogsDtails =()=>{
+    return (
+        <div>
+   <h1 style={{marginTop:100}}>BlogsDtails</h1>
+   
+   
+        </div>
+    )
+}
